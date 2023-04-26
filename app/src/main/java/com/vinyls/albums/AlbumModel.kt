778aa2e2a347e02a4722a9278c925a0ff1292d2e
rogive <ie.rodriguez@uniandes.edu.co>
2023-04-25 22:12:07 -0500

    feat: albums list activity

---
+++ app/src/main/java/com/vinyls/albums/AlbumModel.kt
@@ -0,0 +1,10 @@
+package com.vinyls.albums
+
+data class Album(
+    val id: Int,
+    val cover: String,
+    val name: String,
+    val description: String,
+    val releaseDate: String,
+    val genre: String,
+)
