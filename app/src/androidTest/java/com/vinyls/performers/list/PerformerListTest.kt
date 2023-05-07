97b331077c6a6f5151aa668937ae1971075b1bae
rogive-dev <ie.rodriguez@uniandes.edu.co>
2023-05-07 07:17:52 -0500

    VN010: Performer List (#41)
    Desarrollar vista para consultar el listado de artistas
    Integrar servicio web en vista para consultar catálogo de artistas
    Implementar pruebas en vista para consultar catálogo de artistas

---
+++ app/src/androidTest/java/com/vinyls/performers/list/PerformerListTest.kt
@@ -0,0 +1,44 @@
+package com.vinyls.performers.list
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import com.vinyls.MainActivity
+import com.vinyls.R
+import com.vinyls.performers.Performer
+import com.vinyls.atPosition
+
+var performers: Array<Performer> = arrayOf(
+    Performer(name = "Queen", image = "https://pm1.narvii.com/6724/a8b29909071e9d08517b40c748b6689649372852v2_hq.jpg", id = 1),
+    Performer(name = "Soda Stereo", image = "https://upload.wikimedia.org/wikipedia/commons/6/6f/CeratiAlbertiBosio.jpg", id = 2),
+    Performer(name = "Maroon 5", image = "https://source.boomplaymusic.com/group10/M00/01/30/1579a2e6e3fa4da380b51afb2e42c553_320_320.png", id = 3),
+)
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class PerformerListTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun ensurePerformerListWork() {
+        onView(withId(R.id.navigation_performers)).perform(click())
+        Thread.sleep(4000)
+        for (i in performers.indices) {
+            onView(withId(R.id.performer_list))
+                .check(matches(atPosition(i, hasDescendant(withText(performers[i].name)))))
+        }
+    }
+}
---
+++ app/src/main/java/com/vinyls/performers/list/PerformerListAdapter.kt
@@ -0,0 +1,46 @@
+package com.vinyls.performers.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.net.toUri
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+
+import com.vinyls.R
+import com.vinyls.performers.Performer
+
+class PerformerListAdapter(
+    private val dataset: List<Performer>
+) : RecyclerView.Adapter<PerformerListAdapter.PerformerViewHolder>() {
+
+    class PerformerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imageView: ImageView = view.findViewById(R.id.performer_image)
+        val nameView: TextView = view.findViewById(R.id.performer_name)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PerformerViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_performer_item, parent, false)
+        return PerformerViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: PerformerViewHolder, position: Int) {
+        val item = dataset[position]
+
+        holder.nameView.text = item.name
+        item.image.let {
+            val imgUri = item.image.toUri().buildUpon().scheme("https").build()
+            holder.imageView.load(imgUri) {
+                placeholder(R.drawable.loading_animation)
+                error(R.drawable.ic_broken_image_black_24dp)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+}
